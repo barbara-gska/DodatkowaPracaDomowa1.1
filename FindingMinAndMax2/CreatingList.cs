d788af249c0ee8cf0c2e74da09888e04fe7088f6
Barbara Gska <pacuszka.barbara@gmail.com>
2020-07-30 12:32:23 +0200

    Adding project files

---
+++ FindingMinAndMax2/CreatingList.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindingMinAndMax2
+{
+    public static class CreatingList
+    {
+        public static List<int> CreateList(string phrase)
+        {
+            var numbers = new List<int>();
+
+            foreach (var number in phrase.Split(' '))
+            {
+                numbers.Add(int.Parse(number));
+            }
+
+            return numbers;
+        }
+    }
+}
---
+++ FindingMinAndMax2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindingMinAndMax2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj ciąg liczb rozdzielając je spacją");
+
+            var numbers = CreatingList.CreateList(Console.ReadLine());
+
+
+            var min = numbers[0];
+            var max = numbers[0];
+
+            foreach (var number in numbers)
+            {
+                if (number < min)
+                    min = number;
+
+                if (number > max)
+                    max = number;
+            }
+
+
+            Console.WriteLine($"{max.ToString()} {min.ToString()}");
+
+            Console.ReadLine();
+        }
+    }
+}
